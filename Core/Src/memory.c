ec1cf3d2d80c151d18281846b04873bb2a23a5a8
Yaroslav Sukhovey <yaroslav.sukhovey@gmail.com>
2023-08-08 13:30:29 +0300

    Removed delay

--- Core/Src/memory.c
+++ Core/Src/memory.c
@@ -4,7 +4,6 @@
 
 void WriteToFlash(uint32_t Value)
 {
-	uint8_t i;
 	uint32_t pageAdr;
 
 	pageAdr = FIRMWARE_PAGE_OFFSET;                    // Адрес страницы памяти
@@ -21,10 +20,10 @@ void WriteToFlash(uint32_t Value)
 	HAL_FLASH_Lock();
 }
 
-void Flash_ReadParams(void) {
-
-	uint32_t *source_adr = (uint32_t *)(FIRMWARE_PAGE_OFFSET);   // Определяем адрес, откуда будем читать
-	uint32_t dest_adr;                                          // Определяем адрес, куда будем писать
-	(dest_adr) = *(__IO uint32_t*)(source_adr);  
-    HAL_Delay;                  // Само чтение
+void Flash_ReadParams(void) 
+{
+	uint32_t *source_adr = (uint32_t*)FIRMWARE_PAGE_OFFSET;   // Определяем адрес, откуда будем читать
+	uint32_t dest_adr;                                        // Определяем адрес, куда будем писать
+	
+    dest_adr = *(__IO uint32_t*)(source_adr);  
 }
