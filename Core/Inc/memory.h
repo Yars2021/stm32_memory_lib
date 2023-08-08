71238719ce37bfa2011203065f3ae698aa1edf30
Ja_Vani <Sonic_Van@mail.ru>
2023-08-08 12:54:43 +0300

    header update

--- Core/Inc/memory.h
+++ Core/Inc/memory.h
@@ -1,5 +1,11 @@
 #ifndef _MEMORY_H_
 #define _MEMORY_H_
 
+typedef enum    {
+    Flash_Memory = 0
+}   Device_type;
+
+HAL_StatusTypeDef readmem(Device_type dev_t, char *buff, size_t len);
+HAL_StatusTypeDef writemem(Device_type dev_t, char *buff, size_t len);
 
 #endif /* _MEMORY_H_ */
