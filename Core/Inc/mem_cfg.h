c334ec62b69348d4956e9ffe516c342cda830667
Yaroslav Sukhovey <yaroslav.sukhovey@gmail.com>
2023-09-21 13:44:54 +0300

    Memory modules can be switched on and off in cfg

--- Core/Inc/mem_cfg.h
+++ Core/Inc/mem_cfg.h
@@ -1,6 +1,8 @@
 #ifndef _MEM_CFG_
 #define _MEM_CFG_
 
+#define FLASH_MEM
+#define EEPROM_MEM
 #define EEPROM_AT24C_I2C1 256, &hi2c1, 0x010
 
 #endif /* _MEM_CFG_ */
--- Core/Src/memory.c
+++ Core/Src/memory.c
@@ -1,9 +1,14 @@
 #include "include.h"
 
+#ifdef FLASH_MEM
 #include "flash_mem.h"
+#endif
+
+#ifdef EEPROM_MEM
 #include "eeprom_mem.h"
 
 extern I2C_HandleTypeDef hi2c1;
+#endif 
 
 #ifdef EEPROM_AT24C_I2C1
 EEPROM_device_t eeprom_dev_i2c1;
@@ -19,9 +24,14 @@ void init_mem(void)
 HAL_StatusTypeDef readmem(Device_type dev_t, void *device, size_t addr, char *buff, size_t len) {
     switch (dev_t) {
         case Flash_Memory:
+            #ifdef FLASH_MEM
             return flash_readmem(buff, len, addr);
+            #endif
+
         case EEPROM_Memory:
+            #ifdef EEPROM_MEM
             return eeprom_readmem((EEPROM_device_t*)device, buff, len, addr);
+            #endif
         
         default:
             return HAL_OK;
@@ -31,9 +41,14 @@ HAL_StatusTypeDef readmem(Device_type dev_t, void *device, size_t addr, char *bu
 HAL_StatusTypeDef writemem(Device_type dev_t, void *device, size_t addr, char *buff, size_t len) {
     switch (dev_t) {
         case Flash_Memory:
+            #ifdef FLASH_MEM
             return flash_writemem(buff, len, addr);
+            #endif
+
         case EEPROM_Memory:
+            #ifdef EEPROM_MEM
             return eeprom_writemem((EEPROM_device_t*)device, buff, len, addr);
+            #endif
 
         default:
             return HAL_OK;
