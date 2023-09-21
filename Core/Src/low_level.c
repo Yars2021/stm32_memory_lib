a6817c7bfebddb7dbaa31dc09d34485461a57123
Yaroslav Sukhovey <yaroslav.sukhovey@gmail.com>
2023-09-21 12:48:23 +0300

    Moved I2C init to low_level

--- Core/Src/low_level.c
+++ Core/Src/low_level.c
@@ -2,12 +2,16 @@
 
 static void init_rcc (void);
 static void init_gpio (void);
+static void i2c1_init(void);
+
+I2C_HandleTypeDef hi2c1;
 
 void initLowLevel (void)
 {
     HAL_Init();
     init_rcc();
     init_gpio();
+    i2c1_init();
 }
 
 static void init_rcc (void)
@@ -47,4 +51,35 @@ static void init_gpio (void)
     AFIO->MAPR |= AFIO_MAPR_SWJ_CFG_1;
 
     pin_init(PIN_LED_GREEN);
+
+    GPIO_InitTypeDef GPIO_InitStructure = {0};
+
+    __HAL_RCC_GPIOB_CLK_ENABLE();
+
+    GPIO_InitStructure.Mode = GPIO_MODE_AF_OD;
+    GPIO_InitStructure.Speed = GPIO_SPEED_FREQ_HIGH;
+    GPIO_InitStructure.Pull = GPIO_PULLUP;
+    GPIO_InitStructure.Pin = GPIO_PIN_8 | GPIO_PIN_9; // I2C1: SCL | SDA
+    HAL_GPIO_Init(GPIOB, &GPIO_InitStructure);
+}
+
+static void i2c1_init(void)
+{
+    __HAL_AFIO_REMAP_I2C1_ENABLE();
+    __HAL_RCC_I2C1_CLK_ENABLE();
+
+    hi2c1.Instance = I2C1;
+    hi2c1.Init.ClockSpeed = 100000;
+    hi2c1.Init.DutyCycle = I2C_DUTYCYCLE_2;
+    hi2c1.Init.OwnAddress1 = 0;
+    hi2c1.Init.AddressingMode = I2C_ADDRESSINGMODE_7BIT;
+    hi2c1.Init.DualAddressMode = I2C_DUALADDRESS_DISABLE;
+    hi2c1.Init.OwnAddress2 = 0;
+    hi2c1.Init.GeneralCallMode = I2C_GENERALCALL_DISABLE;
+    hi2c1.Init.NoStretchMode = I2C_NOSTRETCH_DISABLE;
+    
+    if (HAL_I2C_Init(&hi2c1) != HAL_OK)
+    {
+
+    }
 }
--- Core/Src/main.c
+++ Core/Src/main.c
@@ -2,7 +2,8 @@
 
 #include "include.h"
 
-I2C_HandleTypeDef hi2c1;
+extern I2C_HandleTypeDef hi2c1;
+
 EEPROM_device_t eeprom_dev;
 
 HAL_StatusTypeDef w_stat, r_stat;
@@ -14,39 +15,6 @@ char read_mem[256] = {0};
 
 size_t addr = 0xA;
 
-void i2c_interface_init(I2C_HandleTypeDef *i2c_handle)
-{
-    if (!i2c_handle) return;
-
-    GPIO_InitTypeDef GPIO_InitStructure = {0};
-
-    __HAL_RCC_GPIOB_CLK_ENABLE();
-
-    GPIO_InitStructure.Mode = GPIO_MODE_AF_OD;
-    GPIO_InitStructure.Speed = GPIO_SPEED_FREQ_HIGH;
-    GPIO_InitStructure.Pull = GPIO_PULLUP;
-    GPIO_InitStructure.Pin = GPIO_PIN_8 | GPIO_PIN_9; // SCL | SDA
-    HAL_GPIO_Init(GPIOB, &GPIO_InitStructure);
-
-    __HAL_AFIO_REMAP_I2C1_ENABLE();
-    __HAL_RCC_I2C1_CLK_ENABLE();
-
-    i2c_handle->Instance = I2C1;
-    i2c_handle->Init.ClockSpeed = 100000;
-    i2c_handle->Init.DutyCycle = I2C_DUTYCYCLE_2;
-    i2c_handle->Init.OwnAddress1 = 0;
-    i2c_handle->Init.AddressingMode = I2C_ADDRESSINGMODE_7BIT;
-    i2c_handle->Init.DualAddressMode = I2C_DUALADDRESS_DISABLE;
-    i2c_handle->Init.OwnAddress2 = 0;
-    i2c_handle->Init.GeneralCallMode = I2C_GENERALCALL_DISABLE;
-    i2c_handle->Init.NoStretchMode = I2C_NOSTRETCH_DISABLE;
-    
-    if (HAL_I2C_Init(i2c_handle) != HAL_OK)
-    {
-
-    }
-}
-
 void test_1_byte(void)
 {
     w_stat = eeprom_write_byte(&eeprom_dev, write_short_mem, addr);
@@ -57,8 +25,6 @@ void test_1_byte(void)
 int main (void)
 {
     initLowLevel();
-
-    i2c_interface_init(&hi2c1);
 
     eeprom_device_init(&eeprom_dev, AT24C256, &hi2c1, 0x0010);
 
