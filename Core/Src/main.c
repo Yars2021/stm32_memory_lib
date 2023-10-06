00ccc24598843505b4777c3cbcc0b79a0d282149
Ja_Vani <Sonic_Van@mail.ru>
2023-10-06 11:14:12 +0300

    last

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -34,8 +34,8 @@ int main (void)
         // HAL_Delay(100);
         // readmem(&eeprom_dev_0, addr, read_mem, 255);
 
-        writemem(&n25q_dev_0, addr, write_long_mem, 255);
+        writemem(&n25q_dev_0, addr, write_short_mem, 13);
         HAL_Delay(100);
-        readmem(&n25q_dev_0, addr, read_mem, 255);
+        readmem(&n25q_dev_0, addr, read_mem, 13);
     }
 }
